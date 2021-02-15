181f1ab9f891cadbb4bb203872dc3213f260a035
Yapsailonn <72209890+Yapsailonn@users.noreply.github.com>
2021-02-15 16:12:37 +0100

    usercommit

--- server.js
+++ server.js
@@ -4,29 +4,36 @@
 const express = require('express');
 var app = express();
 var sql = require("mssql");
-
+var result;
 a = async () => {
 
     try {
         await sql.connect('mssql://sogei\\sogeiadmin:pa$$w0rd2018@10.207.234.104/InpsIntranet');
-        const result = await sql.query`select * from documenti`;
-        console.dir(sql);
+        result = await sql.query`select * from documenti`;
+
         console.dir(result);
 
     }
     catch (err) {
         console.log(err);
     }
+
 };
 
 
-a();
 
 
 
 
+app.get("/", async (req, res) => {
+    await a();
+    //console.dir(result);
+    res.send(result.recordset);
+})
 
-// const PORT = 3000;
-// var server = app.listen(PORT, function () {
-//     console.log(`Server is running   http://localhost:${PORT}`);
-// })
+
+
+const PORT = 3000;
+var server = app.listen(PORT, function () {
+    console.log(`Server is running   http://localhost:${PORT}`);
+})
